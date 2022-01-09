6f4cfd7ea4d276f4bf3256e1cf2a5272241b1813
Tyrone Williams <tyrone@Tyrones-MacBook-Air.local>
2022-01-09 19:31:33 +0000

    battle set up

--- lib/app.rb
+++ lib/app.rb
@@ -1,22 +1,37 @@
-require 'sinatra'
-require 'sinatra/reloader' if development?
+require 'sinatra/base'
+require 'sinatra/reloader'
+
+class MyApp < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  # our routes would go here
+
+  # # Start the server if this file is executed directly (do not change the line below)
+  
+# require 'sinatra'
+# require 'sinatra/reloader' if development?
 
 
-get '/' do "hello world" end
-get '/secret' do "hello! ruby" end
-get '/route_a' do "hello route_a" end
+  get '/' do "hello world" end
+  get '/secret' do "hello! ruby" end
+  get '/route_a' do "hello route_a" end
 
-get '/random-cat' do
-  @name = ["Amigo", "Misty", "Almond"].sample
-  erb(:index)
-end
+  get '/random-cat' do
+    @name = ["Amigo", "Misty", "Almond"].sample
+  e rb(:index)
+  end
 
-post '/named-cat' do
-  p params
-  @name = params[:name]
-  erb(:index)
-end
+  post '/named-cat' do
+    p params
+    @name = params[:name]
+    erb(:index)
+  end
 
-get '/cat-form' do
-  erb(:cat_form)
+  get '/cat-form' do
+    erb(:cat_form)
+  end
+
+  run! if app_file == $0
 end
